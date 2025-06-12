7e35d29bd5fa0cd5dbe1fbf48e85a7fdff56e37f
aquapi <aquaplmc@gmail.com>
2025-06-12 18:09:21 +0700

    Update

--- src/sse.ts
+++ src/sse.ts
@@ -1,100 +1,56 @@
-type Emitter = [p: Promise<void>, res: () => void, fn: (res: () => void) => void, ...chunks: any[]];
-
-const resolvedPromise = Promise.resolve();
+// A list of readable stream controller
+type Channel = [number, ReadableStreamDefaultController<any>][];
 
 /**
- * Create an event emitter
+ * Create an event channel
  */
-export const emitter = (): Emitter => {
-  const chunks: any[] = [null, null, null];
-  chunks[0] = new Promise(chunks[2] = (res: any) => {
-    chunks[1] = res;
-  });
-  return chunks as Emitter;
-}
+export const channel = (): Channel => [];
 
 const options = {
   headers: [
     ['Content-Type', 'text/event-stream'],
-    ['Cache-Control', 'no-cache']
-  ]
+    ['Cache-Control', 'no-cache'],
+  ],
 } satisfies ResponseInit;
 
 /**
  * Event start
  * @param name
  */
-export const startEvent = (name: string): string => 'event: ' + name + '\ndata: ';
+export const startEvent = (name: string): string =>
+  'event: ' + name + '\ndata: ';
 
 /**
  * Create an event stream
  * @param signal
  * @param emitter - The event emitter
  */
-export const stream = (signal: AbortSignal, emitter: Emitter): ReadableStream => {
-  const st = async (c: ReadableStreamDefaultController<any>): Promise<any> => {
-    if (!signal.aborted) {
-      await emitter[0];
-      emitter[0] = new Promise(emitter[2]);
+export const stream =
+  (chan: Channel): ((req: Request) => Response) =>
+  (req) =>
+    new Response(
+      new ReadableStream({
+        start: (c) => {
+          const tmp: Channel[number] = [chan.length, c];
+          chan.push(tmp);
 
-      for (let i = 3; i < emitter.length; i++)
-        c.enqueue(emitter[i]);
-      emitter.length = 3;
+          req.signal.addEventListener('abort', () => {
+            const last = chan.pop()!;
 
-      return st(c);
-    }
-
-    c.close();
-  }
-
-  return new ReadableStream({
-    start: st
-  });
-}
-
-/**
- * Create an event stream for Bun
- * @param signal
- * @param emitter - The event emitter
- */
-export const bunStream = (signal: AbortSignal, emitter: Emitter): ReadableStream => {
-  const st = async (c: ReadableStreamDirectController): Promise<any> => {
-    if (!signal.aborted) {
-      await emitter[0];
-      emitter[0] = new Promise(emitter[2]);
-
-      for (let i = 3; i < emitter.length; i++)
-        c.write(emitter[i]);
-      emitter.length = 3;
-
-      return st(c);
-    }
-
-    c.close();
-  }
-
-  // @ts-ignore
-  return new ReadableStream({
-    type: 'direct',
-    pull: st
-  });
-}
+            // Replace current item in the position
+            if (chan.length > 0) chan[(last[0] = tmp[0])] = last;
+          });
+        },
+      }),
+      options,
+    );
 
 /**
- * Create a fetch handler
- * @param emitter
+ * Send a chunk to the channel
  */
-export const handler = (emitter: Emitter): (req: Request) => Response => globalThis.Bun != null
-  ? (req) => new Response(bunStream(req.signal, emitter), options)
-  : (req) => new Response(stream(req.signal, emitter), options);
-
-/**
- * @param emitter
- */
-export const flush = (emitter: Emitter): Promise<void> => {
-  emitter[1]();
-  return resolvedPromise;
-}
+export const send = (chan: Channel, chunk: any): void => {
+  for (let i = 0; i < chan.length; i++) chan[i][1].enqueue(chunk);
+};
 
 /**
  * Send to end the data
